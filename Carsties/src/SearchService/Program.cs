7ddcd30a555b16fbfdc3b2a43e49d4acb3163a11
Adir Noyman <ofahol2007@hotmail.com>
2023-08-19 17:16:18 +0300

    Added MongoDB connection + Item model

--- Carsties/src/SearchService/Program.cs
+++ Carsties/src/SearchService/Program.cs
@@ -1,3 +1,7 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,5 +16,15 @@ var app = builder.Build();
 app.UseAuthorization();
 
 app.MapControllers();
+
+// Initialize the MongoDB 
+await DB.InitAsync("SearchDB", MongoClientSettings.FromConnectionString(builder.Configuration.GetConnectionString("MongoDbConnection")));
+
+// Create the indexes for the search functionality
+await DB.Index<Item>()
+.Key(x => x.Make, KeyType.Text)
+.Key(x => x.Model, KeyType.Text)
+.Key(x => x.Color, KeyType.Text)
+.CreateAsync();
 
 app.Run();
